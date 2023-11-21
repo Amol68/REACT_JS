8b18cb83d255353a889e624c50b8a4c3e2c77d8f
noName <mygeat07@gmail.com>
2023-11-21 23:53:05 +0530

    js file

---
+++ app.js
@@ -0,0 +1,18 @@
+/* const heading = React.createElement("h1" , 
+{id:"heading"}, 
+"Who's Gonna Carry The Boats and The Loads!!!");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am Head"),
+    React.createElement("h2", {}, "I am Sub-Head"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
